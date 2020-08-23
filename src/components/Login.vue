d1ad1f88ef279cfcb2e75b02039b1e9e2efd08e0
JP Aguirre <jp.aguirre90@gmail.com>
2020-08-22 22:18:33 -0400

    etapa 2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,5 @@
 <template>
 <div>
-    <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-        </li>
-    </ul>
-    </div>
-    </nav>
     <div class="container modal-dialog">        
         <div class="row">            
             <div class="col-md-12">
@@ -92,7 +75,8 @@ export default {
     },
     methods:{
         login(){
-            console.log('login')
+            const datos = {email: this.login_email, password: this.login_password};
+            this.$store.dispatch('login', datos)
         },
         register(){
           const repeat = document.getElementById('repeat');
--- src/main.js
+++ src/main.js
@@ -25,22 +25,47 @@ const store = new Vuex.Store({
       }
   },
   actions: {
-      async register(context, datos) {
-          try {
-              await firebase.auth().createUserWithEmailAndPassword(datos.email, datos.password);
-              let newUser = firebase.auth().currentUser;  // after user is created, it is retrieved
-              await newUser.updateProfile({
-                displayName: datos.name
-              });      
-              context.commit('setUser', {email: datos.email, name: datos.name});   // .uid has the user's unique identifier
-              context.commit('setError', null);
-              router.push("/")
-          }
-          catch(error) {
-              console.log(error);
-              context.commit('setError', error);
-              context.commit('setUser',null);
-          }
+    async register(context, datos) {
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(datos.email, datos.password);
+            let newUser = firebase.auth().currentUser;  // after user is created, it is retrieved
+            await newUser.updateProfile({
+              displayName: datos.name
+            });      
+            context.commit('setUser', {email: datos.email, name: datos.name});   // .uid has the user's unique identifier
+            context.commit('setError', null);
+            router.push("/")
+        }
+        catch(error) {
+            console.log(error);
+            context.commit('setError', error);
+            context.commit('setUser',null);
+        }
+    },
+    login(context,datos){
+        firebase.auth().signInWithEmailAndPassword(datos.email, datos.password)
+        .then(function(response){
+          console.log(response)
+          console.log(datos.email)
+          var name= firebase.auth().currentUser.displayName;
+          var id= firebase.auth().currentUser.uid;
+          context.commit('setError', null);
+          context.commit('setUser',{email:datos.email,name:name,id:id});
+          router.push('/');
+        })
+        .catch(function(error){
+          context.commit('setError', error);
+          context.commit('setUser', null);
+        })
+      },
+      Salir(context){
+        firebase.auth().signOut()
+        .then(function(response){
+          console.log(response)
+          context.commit('set_error', null);
+          context.commit('set_user', null);
+          router.push('/login');
+        })
       }
   }
 })
